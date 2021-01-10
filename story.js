f2f0492cbfe2fde62aef2b16450da5b005ee7601
devolisto <devolisto@gmail.com>
2021-01-10 12:44:12 +0300

    did the variables task

---
+++ story.js
@@ -0,0 +1,5 @@
+const name = "Mohammed";
+const age = 27;
+const hobby = "horse riding";
+
+console.log(`Mr ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
